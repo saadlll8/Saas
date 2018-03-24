164ab17495fd899283b3842f259493331f2dc37f
saadlll8 <37086918+saadlll8@users.noreply.github.com>
2018-03-23 20:20:31 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide(800);
+    });
+});
+function myFunction() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("mystyle");
+}
+
+
+
+
+function myFunction() {
+    var signed = document.getElementById("hi");
+   signed.classList.toggle("show");
+   
+ 
+  
+}
